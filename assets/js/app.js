34d9da2d4592a8601de5c811784a43bebb6c984e
thesarfo <ernestsarfo100@gmail.com>
2023-05-11 13:33:46 +0000

    making updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,105 +1,97 @@
-let idGenerated = 1;
-
-// Retrieve todos from localStorage if available
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let idGenarated = 1
 
 function updateTodosCounter() {
-  const todos = document.querySelectorAll('.card.active');
-  const todosCounter = todos.length;
-  counter.innerHTML = todosCounter;
+  const todos = document.querySelectorAll(`.card`)
+  let activeTodos = []
+  for (let todo of todos) {
+    if (todo.classList.contains(`active`)) {
+      activeTodos.push(todo)
+    }
+  }
+  const todosCounter = activeTodos.length
+  counter.innerHTML = todosCounter
 }
 
-todoInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    addTodoToHtml(todoInput.value);
-    idGenerated++;
-    updateTodosCounter();
+todoInput.addEventListener(`keypress`, e => {
+  if (e.key === `Enter`) {
+    addTodoToHtml(todoInput.value)
+    idGenarated++
+
+    updateTodosCounter()
   }
-});
+})
 
 function addTodoToHtml(todoText) {
-  if (todoText !== '') {
-    const dragArea = document.querySelector('.dragArea');
-    const newTodo = document.createElement('div');
-    newTodo.classList.add('card', 'active');
-    newTodo.id = `card${idGenerated}`;
+  if (todoText !== ``) {
+    const dragArea = document.querySelector(`.dragArea`)
+    const newTodo = document.createElement(`div`)
+    newTodo.classList.add(`card`, `active`)
+    newTodo.id = `card${idGenarated}`
     newTodo.innerHTML = `
-      <div class="card_content">
-        <div>
-          <input type="checkbox" class="filteredOnSortable" onclick="toggleCompletedClass('card${idGenerated}')"/>
-          <p class="todo_text">${todoText}</p>
-        </div>
-        <button onclick="deleteTodo('card${idGenerated}')" class="filteredOnSortable">
-          <img src="./assets/svgs/icon-cross.svg" alt="" />
-        </button>
+    <div class="card_content">
+      <div>
+        <input type="checkbox" class="filteredOnSortable" onclick="toggleCompletedClass('card${idGenarated}')"/>
+        <p class="todo_text">${todoText}</p>
       </div>
-      <div class="bottomLine"></div>
-    `;
-    dragArea.appendChild(newTodo);
-    todoInput.value = '';
-
-    // Add the new todo to the todos array
-    todos.push({
-      id: newTodo.id,
-      text: todoText,
-      completed: false
-    });
-
-    // Save todos to localStorage
-    localStorage.setItem('todos', JSON.stringify(todos));
+      
+      <button onclick="deleteTodo('card${idGenarated}')" class="filteredOnSortable">
+        <img src="./assets/svgs/icon-cross.svg" alt="" />
+      </button>
+    </div>
+    
+    <div class="bottomLine"></div>
+  `
+    dragArea.appendChild(newTodo)
+    todoInput.value = ``
   }
 }
 
 function toggleCompletedClass(cardId) {
-  const card = document.querySelector(`#${cardId}`);
-  card.classList.toggle('active');
+  const card = document.querySelector(`#${cardId}`)
+  card.classList.toggle(`active`)
 
-  if (card.classList.contains('completed')) {
-    card.classList.remove('completed');
-    updateTodosCounter();
-    updateTodoStatus(cardId, false);
+  if (card.classList.contains(`completed`)) {
+    card.classList.remove(`completed`)
+    updateTodosCounter(`increment`)
   } else {
-    card.classList.add('completed');
-    updateTodosCounter();
-    updateTodoStatus(cardId, true);
+    card.classList.add(`completed`)
+    updateTodosCounter(`decrement`)
   }
-}
 
-function updateTodoStatus(cardId, completed) {
-  // Find the todo in the todos array and update its completed status
-  const todo = todos.find((item) => item.id === cardId);
-  if (todo) {
-    todo.completed = completed;
-    // Save todos to localStorage
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }
+  updateTodosCounter()
 }
 
 function deleteTodo(cardId) {
-  const card = document.querySelector(`#${cardId}`);
-  card.remove();
+  const card = document.querySelector(`#${cardId}`)
+  card.remove()
 
-  // Remove the todo from the todos array
-  todos = todos.filter((item) => item.id !== cardId);
-
-  // Save todos to localStorage
-  localStorage.setItem('todos', JSON.stringify(todos));
-
-  updateTodosCounter();
+  updateTodosCounter()
 }
 
-// Load todos from localStorage on page load
-function loadTodos() {
-  todos.forEach((todo) => {
-    addTodoToHtml(todo.text);
-    const card = document.querySelector(`#${todo.id}`);
-    if (todo.completed) {
-      card.classList.add('completed');
+function clearCompletedTodos() {
+  const todos = document.querySelectorAll(`.card`)
+  todos.forEach(todo => {
+    if (todo.classList.contains(`completed`)) {
+      todo.remove()
     }
-  });
+  })
 
-  updateTodosCounter();
+  updateTodosCounter()
 }
 
-loadTodos();
+function filterTodos(HtmlClass) {
+  const todos = document.querySelector(`.todos`)
+  todos.classList.remove(`activeFilter`)
+  todos.classList.remove(`completedFilter`)
 
+  todos.classList.add(`${HtmlClass}`)
+}
+
+function addOnClass(buttonId) {
+  const buttons = document.querySelectorAll(`.filter_button`)
+  const button = document.querySelector(`#${buttonId}`)
+  buttons.forEach(button => {
+    button.classList.remove(`on`)
+  })
+  button.classList.add(`on`)
+}
